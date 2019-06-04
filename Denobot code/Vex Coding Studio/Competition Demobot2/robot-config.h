4466042a26d7f9605a5a2b7337a2ca1e12d5aaf2
Brandon Marshall <bjm321@gmail.com>
2019-06-04 15:50:05 -0700

    update one motor's directions

--- Denobot code/Vex Coding Studio/Competition Demobot2/robot-config.h
+++ Denobot code/Vex Coding Studio/Competition Demobot2/robot-config.h
@@ -2,5 +2,5 @@ vex::brain Brain;
 vex::controller Controller1 = vex::controller();
 vex::motor BackRightMotor = vex::motor(vex::PORT16,vex::gearSetting::ratio6_1,false);
 vex::motor BackLeftMotor = vex::motor(vex::PORT15,vex::gearSetting::ratio6_1,false);
-vex::motor FrontRightMotor = vex::motor(vex::PORT6,vex::gearSetting::ratio6_1,false);
+vex::motor FrontRightMotor = vex::motor(vex::PORT6,vex::gearSetting::ratio6_1,true);
 vex::motor FrontLeftMotor = vex::motor(vex::PORT5,vex::gearSetting::ratio6_1,false);
