4c68cd71724623273eaacb6b4badcfeba4277832
bjm321 <bjm321@users.noreply.github.com>
2019-09-18 18:55:18 -0700

    g

--- EDR/V5/Test-Code/Vex Code/sd_card_write_test_3/src/main.cpp
+++ EDR/V5/Test-Code/Vex Code/sd_card_write_test_3/src/main.cpp
@@ -11,45 +11,79 @@
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
-vex::motor      motor1(vex::PORT20, vex::gearSetting::ratio18_1, true);
+vex::brain Brain;
+vex::motor motor1(vex::PORT20, vex::gearSetting::ratio18_1, true);
 vex::controller con(vex::controllerType::primary);
 // define your global instances of motors and other devices here
 
-
 #include <cstdio>
-#include <iostream>
+#include <fstream>
 #include <iomanip>
-#include <fstream>
-#include <fstream>
+#include <iostream>
+#include <iostream> // std::cout
+#include <fstream>  // std::ifstream, std::ofstream
+#include <sstream>  // std::sstream
+#include <string>   // std::string
+#include <ctime>    // std::strftime, std::localtime, std::time
+#include <array>    // std::array<T>
+
+using std::stringstream;
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+
+using std::localtime;
+using std::strftime;
+using std::time;
+
+using std::string;
+using std::array;
+
 
 bool fexists(const char *filename) {
-	std::ifstream ifile(filename);
-	return (bool)ifile;
+  std::ifstream ifile(filename);
+  return (bool)ifile;
 }
 std::ofstream ofs;
 
 int main() {
-	if( Brain.SDcard.isInserted() ) {
-		if(fexists("run3.csv")){
-			Brain.Screen.printAt(10, 40, "need a new file");
-		}
-		else{
-			// create a file with long filename
-			ofs.open("Run3.csv", std::ofstream::out);
-			ofs << "time,battery,,motor1,,,,,,,,\r\n";
-			ofs << "senors,capacity,temperature,Position,Velocity (calculated),Current,Voltage,Power,Torque (calculated),Efficiency (calculated)\r\n";
-			for(int i=1; i<=6; i++){
-				ofs << Brain.timer(msec) << "," << Brain.Battery.capacity() << "," << Brain.Battery.temperature() << "," << i + 8 << "," << i * 3 << "," << i * i << "," << i + 1 << "," << i *i *i << "," << i << "," << i-5 <<"\n";
-vex::task::sleep(1);
-			}
-      ofs.close();
-			Brain.Screen.printAt(10, 40, "done");
-		}
+  int i = 1;
+  bool nameTest = true;
+  if (Brain.SDcard.isInserted()) {
 
-	}
+    stringstream s;
+        s << "run (" << i << ").csv";
+        const string filename = s.str();
+        ofstream ofs(filename);
 
-	else {
-		Brain.Screen.printAt(10, 40, "No SD Card");
-	}
+    while (nameTest == true) {
+
+      if (fexists(s.str())) {
+        i++;
+      }
+
+      else {
+        nameTest = false;
+      }
+    }
+    // create a file with long filename
+    ofs.open("Run3.csv", std::ofstream::out);
+    ofs << "time,battery,,motor1,,,,,,,,\r\n";
+    ofs << "senors,capacity,temperature,Position,Velocity "
+           "(calculated),Current,Voltage,Power,Torque (calculated),Efficiency "
+           "(calculated)\r\n";
+    for (int i = 1; i <= 6; i++) {
+      ofs << Brain.timer(msec) << "," << Brain.Battery.capacity() << ","
+          << Brain.Battery.temperature() << "," << i + 8 << "," << i * 3 << ","
+          << i * i << "," << i + 1 << "," << i * i * i << "," << i << ","
+          << i - 5 << "\n";
+      vex::task::sleep(1);
+    }
+    ofs.close();
+    Brain.Screen.printAt(10, 40, "done");
+  }
+
+  else {
+    Brain.Screen.printAt(10, 40, "No SD Card");
+  }
 }
--- EDR/V5/Test-Code/Vex Code/sd_card_write_test_5/src/main.cpp
+++ EDR/V5/Test-Code/Vex Code/sd_card_write_test_5/src/main.cpp
@@ -11,41 +11,39 @@
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
-vex::motor      motor1(vex::PORT20, vex::gearSetting::ratio18_1, true);
+vex::brain Brain;
+vex::motor motor1(vex::PORT20, vex::gearSetting::ratio18_1, true);
 vex::controller con(vex::controllerType::primary);
-vex::gyro Gyro1 = vex::gyro( Brain.ThreeWirePort.B );
-vex::limit Limit1 = vex::limit( Brain.ThreeWirePort.A );
-vex::light Light1 = vex::light( Brain.ThreeWirePort.C );
-vex::line Line1 = vex::line( Brain.ThreeWirePort.D );
-vex::pot Pot1 = vex::pot( Brain.ThreeWirePort.E );
-vex::sonar Sonar1 = vex::sonar( Brain.ThreeWirePort.F );
+vex::gyro Gyro1 = vex::gyro(Brain.ThreeWirePort.B);
+vex::limit Limit1 = vex::limit(Brain.ThreeWirePort.A);
+vex::light Light1 = vex::light(Brain.ThreeWirePort.C);
+vex::line Line1 = vex::line(Brain.ThreeWirePort.D);
+vex::pot Pot1 = vex::pot(Brain.ThreeWirePort.E);
+vex::sonar Sonar1 = vex::sonar(Brain.ThreeWirePort.F);
 // define your global instances of motors and other devices here
 
 // A global instance of vex::competition
 vex::competition Competition;
 
-#include <iostream> // std::cout
+// -*- mode: c++; compile-command: "g++ -Wall -std=c++11 -o
+// test-write-csv-file-with-dates test-write-csv-file-with-dates.cpp" -*-
 #include <fstream>  // std::ifstream, std::ofstream
+#include <iostream> // std::cout
 #include <sstream>  // std::sstream
 #include <string>   // std::string
-#include <ctime>    // std::strftime, std::localtime, std::time
-#include <array>    // std::array<T>
+#include <vector>   // std::vector<T>
 
-using std::stringstream;
-using std::ifstream;
-using std::ofstream;
+//#include <sys/types.h>
+#include <dirent.h>
 using std::cout;
-
-using std::localtime;
-using std::strftime;
-using std::time;
-
+using std::ifstream;
+using std::ios;
+using std::ofstream;
 using std::string;
-using std::array;
+using std::stringstream;
 
-// Uncomment to simulate calling isInserted() and printAt() without a cortex.
-// struct {
+// Uncomment to simulate calling isInserted() and printAt() without a
+// cortex. struct {
 //         struct {
 //                 bool isInserted() const { return true; }
 //         } SDcard;
@@ -55,119 +53,130 @@ using std::array;
 //                 }
 //         } Screen;
 // } Brain;
-// Returns the current time in RFC 3339 format (YYYY-MM-DDTHH:MM:SS ZoneAbbrev).
-string getCurrentTime() {
-	const size_t MAX_LENGTH  = 50;
-	array<char, MAX_LENGTH> buffer;
-	time_t now               = time(nullptr);
-	size_t charactersWritten = strftime(buffer.data(), buffer.size(), "%F %H_%M_%S %Z", localtime(&now));
-
-	return string(buffer.data(), charactersWritten);
-}
-bool fexists(const char *filename) {
-	std::ifstream ifile(filename);
-	return (bool)ifile;
-}
-int getCount(){
-
-	int count = 1;
-
-	while (fexists(s.str().c_str())){
-
-		if(fexists(s.str().c_str())){
-			count++;
-		}
-		else {
-			return count;
-		}
-	}
+// Returns the index of the next CSV in the file succession
+// prefix+"0"+suffix, prefix+"1"+suffix, and so on, which is NOT already in
+// use.
+int getNextCsvIndex(string prefix, string suffix) {
+  int highestIndex = -1;
+  DIR *directory = ::opendir(".");
+  if (!directory) {
+    return 0;
+  }
+  for (::dirent *entry = ::readdir(directory); entry;
+       entry = ::readdir(directory)) {
+    const string currentFileName = entry->d_name;
+    if (prefix == currentFileName.substr(0, prefix.length()) &&
+        suffix == currentFileName.substr(currentFileName.length() -
+                                         suffix.length())) {
+      // cout << "\"" << currentFileName << "\" matches pattern.\n";
+      // The file matches the format prefix + SOMETHING + suffix.
+      // Extract the SOMETHING bit.
+      string content = currentFileName.substr(
+          prefix.length(),
+          currentFileName.length() - prefix.length() - suffix.length());
+      // Attempt to convert to an integer.
+      stringstream s;
+      s << content;
+      int n;
+      s >> n;
+      if (s) {
+        // Parsed successfully.
+        if (n > highestIndex) {
+          highestIndex = n;
+        }
+      } else {
+        // cout << "  (\"" << content << "\" is not an integer, skipping)\n";
+      }
+    }
+  }
+  closedir(directory);
+  return highestIndex + 1;
 }
 int main() {
-	if (Brain.SDcard.isInserted()) {
+  if (Brain.SDcard.isInserted()) {
+    // Use a new filename for each run.
+    int availableIndex = getNextCsvIndex("run-", ".csv");
+    // Use a new filename for each run (unless two runs are done in the
+    // same second.)
+    stringstream s;
+    s.flags(ios::right);
+    s.fill('0');
+    s.width(6);
+    s << availableIndex;
+    const string filename = "run" + s.str() + ".csv";
+    cout << "Data will be written to " << filename << ".\n";
+    ofstream ofs(filename);
+    if (ofs) {
 
-		// Use a new filename for each run (unless two runs are done in the
-		// same second.)
-		stringstream s;
-		s << "run" << getCount() << ".csv";
-		const string filename = s.str();
-
-		ofstream ofs(filename);
-		if (ofs) {
-			// Write the header row.
-			ofs << "time,battery,,motor1,,,,,,,,,\r\n";
-			ofs << "sensors,capacity,temperature,Position,Velocity (calculated),Current,Voltage,Power,Torque (calculated),Efficiency (calculated),Temperature\r\n";
-			// TODO: Loop through the gathered data and write the other rows here.
-			//ofs << timeMark <<"," << batteryCapacity << "," << batteryTemperature <<",," << motorVelocity << "," << motorCurrent <<",," << motorPower <<"," << motorTorque <<"," << motorEfficiency <<"," << motorTemperature;
-			s.str("");
-			s << "Curret file is  " << filename << "' for writing";
-			Brain.Screen.printAt(10, 40, s.str().c_str());
-		}
-		else {
-			s.str("");
-			s << "Error: Could not open " << filename << "' for writing";
-			Brain.Screen.printAt(10, 40, s.str().c_str());
-
-		}
-	}
-	else {
-		Brain.Screen.printAt(10, 40, "No SD Card");
-	}
+      // Write the header row.
+      ofs << "time,battery,,motor1,,,,,,,,\r\n";
+      ofs << "sensors,capacity,temperature,Position,Velocity "
+             "(calculated),Current,Voltage,Power,Torque "
+             "(calculated),Efficiency (calculated)\r\n";
+      // TODO: Loop through the gathered data and write the other rows here.
+      Brain.Screen.printAt(10, 40, "done\n");
+    } else {
+      s.str("");
+      s << "Error: Could not open " << filename << "' for writing";
+      Brain.Screen.printAt(10, 40, s.str().c_str());
+    }
+  } else {
+    Brain.Screen.printAt(10, 40, "No SD Card");
+  }
 }
-void statin()
-{
+
+void statin() {
   bool motorstat;
-	int batteryCapacity;
-	double timeMark, batteryTemperature, motorRotation, motorVelocity, motorCurrent, motorPower;
-	double motorTorque, motorEfficiency, motorTemperature;
+  int batteryCapacity;
+  double timeMark, batteryTemperature, motorRotation, motorVelocity,
+      motorCurrent, motorPower;
+  double motorTorque, motorEfficiency, motorTemperature;
 
-	motorVelocity = motor1.velocity(vex::velocityUnits::rpm);
-	motorRotation = motor1.rotation(vex::rotationUnits::deg);
-	motorCurrent = motor1.current(vex::currentUnits::amp);
-	motorPower = motor1.power(vex::powerUnits::watt);
-	motorTorque = motor1.torque(vex::torqueUnits::Nm);
-	motorEfficiency = motor1.efficiency(vex::percentUnits::pct);
-	motorTemperature = motor1.temperature(vex::percentUnits::pct);
-	motorstat = motor1.isSpinning(); //bool
+  motorVelocity = motor1.velocity(vex::velocityUnits::rpm);
+  motorRotation = motor1.rotation(vex::rotationUnits::deg);
+  motorCurrent = motor1.current(vex::currentUnits::amp);
+  motorPower = motor1.power(vex::powerUnits::watt);
+  motorTorque = motor1.torque(vex::torqueUnits::Nm);
+  motorEfficiency = motor1.efficiency(vex::percentUnits::pct);
+  motorTemperature = motor1.temperature(vex::percentUnits::pct);
+  motorstat = motor1.isSpinning(); // bool
 
+  batteryTemperature = Brain.Battery.temperature();
+  batteryCapacity = Brain.Battery.capacity();
+  timeMark = Brain.timer(msec);
 
-	batteryTemperature = Brain.Battery.temperature();
-	batteryCapacity = Brain.Battery.capacity();
-	timeMark = Brain.timer(msec);
+  // bool
+  Competition.isCompetitionSwitch();
+  Competition.isFieldControl();
+  Competition.isEnabled();
+  Competition.isAutonomous();
+  Competition.isDriverControl();
 
-	//bool
-	Competition.isCompetitionSwitch();
-	Competition.isFieldControl();
-	Competition.isEnabled();
-	Competition.isAutonomous();
-	Competition.isDriverControl();
+  con.Axis3.position(percentUnits::pct); // int
+  con.ButtonY.pressing();                // bool
 
-	con.Axis3.position(percentUnits::pct); //int
-	con.ButtonY.pressing();//bool
+  // int
+  Gyro1.value(rotationUnits::deg);
+  Gyro1.value(percentUnits::pct);
+  Gyro1.value(analogUnits::mV);
 
-	//int
-	Gyro1.value(rotationUnits::deg);
-	Gyro1.value(percentUnits::pct);
-	Gyro1.value(analogUnits::mV);
+  // int
+  Light1.value(percentUnits::pct);
+  Light1.value(analogUnits::mV);
 
-	//int
-	Light1.value(percentUnits::pct);
-	Light1.value(analogUnits::mV);
+  Limit1.pressing(); // bool
 
-	Limit1.pressing(); //bool
+  // int
+  Line1.value(percentUnits::pct);
+  Line1.value(analogUnits::mV);
 
-	//int
-	Line1.value(percentUnits::pct);
-	Line1.value(analogUnits::mV);
+  // int
+  Pot1.value(rotationUnits::deg);
+  Pot1.value(percentUnits::pct);
+  Pot1.value(analogUnits::mV);
 
-	//int
-	Pot1.value(rotationUnits::deg);
-	Pot1.value(percentUnits::pct);
-	Pot1.value(analogUnits::mV);
-
-	//double
-	Sonar1.distance(distanceUnits::mm);
-	Sonar1.distance(distanceUnits::in);
-	Sonar1.distance(distanceUnits::cm);
-
-
+  // double
+  Sonar1.distance(distanceUnits::mm);
+  Sonar1.distance(distanceUnits::in);
+  Sonar1.distance(distanceUnits::cm);
 }
