33b983706ebe42f032f11a3e2e81be3771990164
bjm321 <bjm321@users.noreply.github.com>
2020-11-02 17:41:07 -0800

    folder re org

--- EDR/V5/Test-Code/Vex Coding Studio/Full Mecanum Drive Tests/robot-config.h
+++
@@ -1,6 +0,0 @@
-vex::brain Brain;
-vex::motor LM1 = vex::motor(vex::PORT20,false);
-vex::motor LM2 = vex::motor(vex::PORT10,false);
-vex::motor RM1 = vex::motor(vex::PORT11,true);
-vex::motor RM2 = vex::motor(vex::PORT1,true);
-vex::controller Controller1 = vex::controller();
